209e7c7c0500513f7cf7a4ab4dddf49d6c58c472
andrelavorente <lavorente.a@gmail.com>
2023-08-21 19:06:42 -0300

    v1

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import autenticar from "./seguranca/Autenticacao.js";
+import session from "express-session";
+import rotaLogin from "./rotas/rotaLogin.js";
+
+//este ip significa que todas a aplicação responde todas as interfaces disponíveis
+const host = "0.0.0.0";
+
+const porta = 3202;
+
+const app = express();
+
+app.use(
+  session({
+    secret: "Minh4ChAveS3crEt4",
+    resave: true,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60 * 30,
+    },
+  })
+);
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static("./public"));
+app.use("/login", rotaLogin);
+app.use(autenticar, express.static("./protegido"));
+
+app.listen(porta, host, () => {
+  console.log("Servidor escutando em", host, porta);
+});
